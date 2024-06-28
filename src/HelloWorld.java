358512ea86accc3b1e69d38f6998b99be706cebf
Nathielle <nathiellec@alunos.unicesumar.edu.br>
2024-06-27 22:42:25 -0300

    Segundo commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!\n");
+        System.out.println("Nova mensagem, edição no programa!\n");
     }
 }
